9e9ca106ce626b6203b5fbcd5e32eaa7a73eb019
quangnhat22 <wuangsotech@gmail.com>
2023-09-02 03:54:50 +0000

    Features/1.0.0/init skeleton

---
+++ app/android/app/src/main/kotlin/xyz/coffeesolution/messenger/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.coffeesolution.messenger.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ resources/lib/resources.dart
@@ -0,0 +1,5 @@
+library resources;
+
+export 'src/export.dart';
+export 'src/fsplash.dart';
+// export 'src/generated/assets/fonts.gen.dart';
