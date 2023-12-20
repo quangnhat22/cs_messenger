e81a3db42a430dec9c1660690a60300c2f258293
quangnhat22 <wuangsotech@gmail.com>
2023-12-20 09:28:55 +0700

    1.0.0 - improve handle chatroom

--- app/lib/features/room_chat/presentation/list_chat_room/controllers/list_chat_room_cubit.dart
+++ app/lib/features/room_chat/presentation/list_chat_room/controllers/list_chat_room_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:app/components/main/listView/controllers/app_list_view_cubit.dart';
+import 'package:app/configs/exts/app_exts.dart';
 import 'package:app/features/room_chat/domain/usecases/get_chat_room_detail_info_uc.dart';
 import 'package:app/features/room_chat/domain/usecases/get_list_chat_room_uc.dart';
 import 'package:app/features/room_chat/domain/usecases/send_message_uc.dart';
@@ -10,11 +11,11 @@ import 'package:utilities/utilities.dart';
 
 @Injectable()
 class ListChatRoomCubit extends AppListViewCubit<ChatRoomModel> {
-  late final GetListChatRoomUseCase _getListFriendUseCase;
+  late final GetListChatRoomUseCase _getListChatRoomUseCase;
   late final GetChatRoomDetailInfoUseCase _getChatRoomDetailInfoUseCase;
   late final SendMessageUseCase _sendMessageUseCase;
 
-  ListChatRoomCubit(this._getListFriendUseCase,
+  ListChatRoomCubit(this._getListChatRoomUseCase,
       this._getChatRoomDetailInfoUseCase, this._sendMessageUseCase) {
     _subGetNewMessage =
         _sendMessageUseCase.getNewMessageStream().listen((messageModel) {
@@ -36,7 +37,7 @@ class ListChatRoomCubit extends AppListViewCubit<ChatRoomModel> {
 
   @override
   Future<AppListResultModel<ChatRoomModel>> onCall(AppParam appParam) async {
-    return _getListFriendUseCase.executeList(
+    return _getListChatRoomUseCase.executeList(
       request: GetListChatRoomParam(
         page: appParam.page,
         pageSize: appParam.pageSize,
@@ -45,6 +46,29 @@ class ListChatRoomCubit extends AppListViewCubit<ChatRoomModel> {
   }
 
   void _onReceiveNewMessage(IMessageModel newMessage) async {
-    await onRefreshCall();
+    try {
+      List<ChatRoomModel> currentListChatRoom = List.from(state.data);
+
+      final indexChatRoom = currentListChatRoom
+          .indexWhere((element) => element.id == newMessage.roomId);
+
+      if (indexChatRoom != -1) {
+        currentListChatRoom.removeAt(indexChatRoom);
+      }
+
+      final messageChatRoom = await _getChatRoomDetailInfoUseCase.executeObj(
+          request: GetChatRoomInfoParam(id: newMessage.roomId));
+
+      if (messageChatRoom.netData != null) {
+        currentListChatRoom.insert(0, messageChatRoom.netData!);
+        emit(state.copyWith(data: [...currentListChatRoom]));
+      }
+    } on AppException catch (e) {
+      AppExceptionExt(
+          appException: e,
+          onError: (e) {
+            Logs.e(e);
+          }).detected();
+    }
   }
 }
